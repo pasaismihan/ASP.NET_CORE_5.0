d65cd06f3c285c108b30a6dc08feebfc3de7797c
pasaismihan <pasa_13142@hotmail.com>
2023-08-16 12:39:54 +0300

    WEB calisma prensibi

--- ASP.NET5.0/Program.cs
+++ ASP.NET5.0/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿// ASP.NET bir web gelistirme mimarisidir
+// ASP.NET ve ASP.NET CORE arasindaki en onemli farklar ASP.NET CORE un open source olmasi ve windows disindaki isletim sistemlerinde de calisma imkaninin olmasi . tabi bunlarin yaninda exstra ozellikler vardir bunlar asagidadir
+// ASP.NET CORE daha performanslidir , cross platformdur , moduler altyapisi vardir , bagimlilik enjeksiyonu(dependency injection) vardir , asenkron islemler vardir , bakimi kolaydir , razor pages vardir
+/* web in temel calisma mantigi ; user(kullanici) client araciligi ile sunucuya request gonderir(client burada browser olabilir userin requesti atabilecegi arac diyebiliriz). Her request gonderdigimiz sitenin bir IP adresi olur
+fakat bu IP adresleri yalnizca rakamlardan olustugu icin akilda kalmasi acisindan Domain aliriz ( metinsel adresler). bu domaine istek atariz yani ilgili sunucuya. Bu sunucuyu evde de olusturabiliriz fakat
+elektrik gitmesi durumu veya bakim durumlari gibi kisimlarda evde sunucu olusturmak maliyetli olacagi icin Hosting hizmeti veren sirketlere bir miktar odeme yaparak 7/24 bu web sitesinin aktif olmasini saglariz
+sunucuya yapilan istek server tarafindan incelenir ve gelen requeste karsilik responce iletir. Bu responcenin icerisinde result vardir yani userin bir islem yapmasina karsilik karsisina cikan sayfadir .*/
