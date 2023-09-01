d0516b912671965618c51ef401a6d270fbb5d719
pasaismihan <pasa_13142@hotmail.com>
2023-09-01 09:46:56 +0300

    DTO Yapilari ve ViewModel

---
+++ DTO_Yapilari&ViewModel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DTO_Yapilari_ViewModel.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
