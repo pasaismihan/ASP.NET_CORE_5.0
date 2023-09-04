ae530cd91a32dc643740bc2ecc251fb5dd9d1b9a
pasaismihan <pasa_13142@hotmail.com>
2023-09-04 15:53:02 +0300

    Environment Variables

---
+++ Environment/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Environment.Models;
+
+namespace Environment.Controllers;
+
+public class HomeController : Controller
+{
+
+    IWebHostEnvironment _webHostEnvironment;
+    public HomeController(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public IActionResult Index()
+    {
+        if (_webHostEnvironment.IsDevelopment())
+        {
+            ViewBag.Env = "Development";
+        }else if (_webHostEnvironment.IsProduction())
+        {
+            ViewBag.Env = "Production";
+        }else if (_webHostEnvironment.IsStaging())
+        {
+            ViewBag.Env = "Staging";
+        }
+      //  var env = _webHostEnvironment.IsDevelopment(); // yani burada development asamasinda mi onu soruyoruz sonuc olarak boolean deger donuyor 
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Environment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Environment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
